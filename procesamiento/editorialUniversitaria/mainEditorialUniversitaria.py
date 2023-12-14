999ec5f7335ef4d47808d7a24e2995a5a7b60c43
enrique gauto sand <elgrancapo666@gmail.com>
2023-12-14 19:19:16 -0300

    prepreprocesamiento

---
+++ procesamiento/editorialUniversitaria/mainEditorialUniversitaria.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from UA_dimensiones_promedios import agregar_columnas,Promedio_desvioEstandar
+from paisUA import agregar_columnaPais
+from guardarToCSV import guararDataframeToCSV
+#ahora agragare agrupacion_canales
+pd.set_option('display.max_columns', None)
+
+analyticsUniversal="editorialUniversitaria_UA_2018-oct4_AgrupacionCanales.csv"
+dir="./datosDataStudio/"
+dfAgrupacionCanales = pd.read_csv(dir+analyticsUniversal)
+
+print(dfAgrupacionCanales.head())
+print(dfAgrupacionCanales.columns)
+data = {'Fecha':[]}
+print(dfAgrupacionCanales.columns)
+dfAgrupacionCanales=agregar_columnas(dfAgrupacionCanales,'Agrupación de canales predeterminada', 'Número de vistas de página')
+dfAgrupacionCanales=Promedio_desvioEstandar(dfAgrupacionCanales,'Agrupación de canales predeterminada')
+
+#ahora agragare sistema operativo
+analyticsUniversal="editorialUniversitaria_UA_2018-oct4_SO.csv"
+dfsisOperativo = pd.read_csv(dir+analyticsUniversal)
+print(dfsisOperativo.columns)
+dfsisOperativo=agregar_columnas(dfsisOperativo,'Sistema operativo','Número de vistas de página')
+print(dfsisOperativo['Sistema operativo (not set)'].unique())
+dfsisOperativo=Promedio_desvioEstandar(dfsisOperativo,'Sistema operativo')
+print('head10',dfsisOperativo.head(10))
+
+##
+
+#    Pais
+
+##
+pd.set_option('display.max_columns', None)
+analyticsUniversal="editorialUniversitaria_UA_2018-oct4_Pais.csv"
+dir="./datosDataStudio/"
+dfPais = pd.read_csv(dir+analyticsUniversal)
+print(dfPais.head())
+print(dfPais.columns)
+dfPais=agregar_columnaPais(dfPais,'País','Número de vistas de página')
+
+#categoria de dispositivo
+analyticsUniversal="editorialUniversitaria_UA_2018-oct4_catDispositivo.csv"
+dir="./datosDataStudio/"
+dfCategoriaDispositivo = pd.read_csv(dir+analyticsUniversal)
+print(dfCategoriaDispositivo.columns)
+dfCategoriaDispositivo=agregar_columnas(dfCategoriaDispositivo,'Categoría de dispositivo','Número de vistas de página')
+dfCategoriaDispositivo=Promedio_desvioEstandar(dfCategoriaDispositivo,'Categoría de dispositivo')
+print('head10',dfCategoriaDispositivo.head(10))
+
+#totales
+analyticsUniversalTotales="editorialUniversitaria_UA_2018-oct4_totales.csv"
+dir="./datosDataStudio/"
+dfTotales = pd.read_csv(dir+analyticsUniversalTotales)
+print(dfCategoriaDispositivo.columns,dfTotales.head() )
+
+
+#unir los dataframes en columnas
+aUnir=[dfTotales,dfPais,dfCategoriaDispositivo,dfsisOperativo,dfAgrupacionCanales]
+print('largo datasets')
+for i in aUnir:
+    print(i.shape)
+dfUnion=pd.concat(aUnir,axis=1)
+
+#guradamos a csv
+salidaCarpeta="./salida/"
+NombreArchivoCSVSalida="UA2018SalidaEditorialUniversitaria.csv"
+guararDataframeToCSV(dfUnion,salidaCarpeta,NombreArchivoCSVSalida)
+
+
+
+
+
---
+++ procesamiento/unamHome/mainUNaM.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from UA_dimensiones_promedios import agregar_columnas,Promedio_desvioEstandar
+from paisUA import agregar_columnaPais
+from guardarToCSV import guararDataframeToCSV
+#ahora agragare agrupacion_canales
+pd.set_option('display.max_columns', None)
+
+analyticsUniversal="informe_UNaM_UA_2018-jun4_agrupacion_canales.csv"
+dir="./datosDataStudio/"
+dfAgrupacionCanales = pd.read_csv(dir+analyticsUniversal)
+
+print(dfAgrupacionCanales.head())
+print(dfAgrupacionCanales.columns)
+data = {'Fecha':[]}
+print(dfAgrupacionCanales.columns)
+dfAgrupacionCanales=agregar_columnas(dfAgrupacionCanales,'Agrupación de canales predeterminada', 'Número de vistas de página')
+dfAgrupacionCanales=Promedio_desvioEstandar(dfAgrupacionCanales,'Agrupación de canales predeterminada')
+
+#ahora agragare sistema operativo
+analyticsUniversal="informe_UNaM_UA_2018-jun4_sistema_operativo.csv"
+dfsisOperativo = pd.read_csv(dir+analyticsUniversal)
+print(dfsisOperativo.columns)
+dfsisOperativo=agregar_columnas(dfsisOperativo,'Sistema operativo','Número de vistas de página')
+print(dfsisOperativo['Sistema operativo (not set)'].unique())
+dfsisOperativo=Promedio_desvioEstandar(dfsisOperativo,'Sistema operativo')
+print('head10',dfsisOperativo.head(10))
+
+##
+
+#    Pais
+
+##
+pd.set_option('display.max_columns', None)
+analyticsUniversal="informe_UNaM_UA_2018-jun4_Pais.csv"
+dir="./datosDataStudio/"
+dfPais = pd.read_csv(dir+analyticsUniversal)
+print(dfPais.head())
+print(dfPais.columns)
+dfPais=agregar_columnaPais(dfPais,'País','Número de vistas de página')
+
+#categoria de dispositivo
+analyticsUniversal="informe_UNaM_UA_2018-jun4_categoria_dispositivo.csv"
+dir="./datosDataStudio/"
+dfCategoriaDispositivo = pd.read_csv(dir+analyticsUniversal)
+print(dfCategoriaDispositivo.columns)
+dfCategoriaDispositivo=agregar_columnas(dfCategoriaDispositivo,'Categoría de dispositivo','Número de vistas de página')
+dfCategoriaDispositivo=Promedio_desvioEstandar(dfCategoriaDispositivo,'Categoría de dispositivo')
+print('head10',dfCategoriaDispositivo.head(10))
+
+#totales
+analyticsUniversalTotales="informe_UNaM_UA_totales_Tabla.csv"
+dir="./datosDataStudio/"
+dfTotales = pd.read_csv(dir+analyticsUniversalTotales)
+print(dfCategoriaDispositivo.columns,dfTotales.head() )
+
+
+#unir los dataframes en columnas
+aUnir=[dfTotales,dfPais,dfCategoriaDispositivo,dfsisOperativo,dfAgrupacionCanales]
+print('largo datasets')
+for i in aUnir:
+    print(i.shape)
+dfUnion=pd.concat(aUnir,axis=1)
+
+#guradamos a csv
+salidaCarpeta="./salida/"
+NombreArchivoCSVSalida="UA2018SalidaUNaM.csv"
+guararDataframeToCSV(dfUnion,salidaCarpeta,NombreArchivoCSVSalida)
+
+
+
+
+
